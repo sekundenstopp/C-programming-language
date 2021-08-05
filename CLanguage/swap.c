0f9b2ea296b5b618c84a11e6786a0faedc3b30d1
Jeongsu Lee <sekundenstopp@gmail.com>
2021-08-05 16:37:44 +0900

    swap

--- CLanguage/swap.c
+++ CLanguage/swap.c
@@ -1,2 +1,35 @@
 #include <stdio.h>
 
+void swap(int a, int b) {
+	//printf("%p %p", &a, &b);
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void swap2(int* a, int* b) {
+	//printf("%p %p", a, b);
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main() {
+	int a = 123;
+	int b = 456;
+
+	//swap
+	int temp = a;
+	a = b;
+	b = temp;
+	//printf("%p %p\n", &a, &b);
+	printf("%d %d\n", a, b);
+
+	swap(a, b);
+	printf("%d %d\n", a, b);
+
+	swap2(&a, &b);
+	printf("%d %d\n", a, b);
+
+	return 0;
+}

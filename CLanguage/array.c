effaba1484c0c76f0f1b9350cfd2103acdbdbd43
Jeongsu Lee <sekundenstopp@gmail.com>
2021-08-05 23:29:36 +0900

    array

---
+++ CLanguage/array.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define MONTHS 12
+
+int main() {
+	/* Monthly temperature*/
+	int high[MONTHS] = { 2,5,11,18,23,27,29,30,26,20,12,4 };
+	int low[MONTHS] = { -7,-5,1,7,13,18,22,22,16,9,2,-5 };
+
+	for (int i = 0; i < MONTHS; ++i) {
+		printf("%d, %d\n", high[i], low[i]);
+	}
+
+	float sum_high = 0.0;
+	float sum_low = 0.0;
+	for (int i = 0; i < MONTHS; i++) {
+		sum_high += high[i];
+		sum_low += low[i];
+	}
+		
+	printf("Average of high = %.2f\n", sum_high / (float)MONTHS);
+	printf("Average of low = %.2f\n", sum_low / (float)MONTHS);
+	
+	//Addresses 
+	printf("%p %p\n", high, &high[0]);
+
+	for (int i = 0; i < MONTHS; i++)
+		printf("%lld\n", (long long)&high[i]); //X64
+	printf("\n");
+
+	//When array is not initialized
+	int not_init[4];
+	for (int i = 0; i < 4; i++)
+		printf("%d ", not_init[i]);
+	printf("\n");
+
+	static int not_init1[4]; // storage class
+	for (int i = 0; i < 4; i++)
+		printf("%d ", not_init1[i]);
+
+	return 0;
+}
